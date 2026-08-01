91e3e0d1f59141dbc0ce3a22af055746c81af7ec
naattxx <31317800+naattxx@users.noreply.github.com>
2026-08-01 22:34:16 +0300

    Add "no disk" screen

--- src/tdisksel.cpp
+++ src/tdisksel.cpp
@@ -1,5 +1,6 @@
 #include "tdisksel.hpp"
 #include "cpptui.hpp"
+#include "src/log.hpp"
 #include <config.h>
 
 using namespace cpptui;
@@ -22,36 +23,61 @@ void testdisk_disk_selection(App &app, int verbose, bool dump, list_disk_t &list
     root->add(std::make_shared<VerticalSpacer>(1));
     root->add(noWarranty);
 
-    auto note = std::make_shared<Static>("");
-
-    for (auto &disk : list_disk)
+    if (list_disk.empty())
     {
-        auto diskBtn = std::make_shared<Button>(disk->description_short(disk));
-        diskBtn->alignment = Alignment::Left;
-        diskBtn->on_hover = [disk, note](bool hover) {
-            if (hover && disk->serial_no)
-            {
-                note->set_text(StyledText("Note: ")
-                                   .colored("Serial number ", Color::Green())
-                                   .colored(disk->serial_no, Color::Green()));
-            }
-            else
-            {
-                note->set_text("");
-            }
-        };
-        root->add(diskBtn);
+        log_critical("No disk found");
+        root->add(std::make_shared<Label>("No hard disk found"));
+#if defined(_WIN32) || defined(__CYGWIN__) || defined(__MINGW32__)
+        root->add(std::make_shared<Paragraph>("You need to be administrator to use TestDisk++.\n"
+                                              "select TestDisk++, right-click and choose \"Run as administrator\"."));
+#elif defined(DJGPP)
+#elif __has_include("unistd.h")
+        root->add(std::make_shared<Static>("You need to be root to use TestDisk++."));
+#endif
+        root->add(std::make_shared<VerticalSpacer>(1));
     }
+    else
+    {
 
-    root->add(std::make_shared<VerticalSpacer>());
+        auto SerialN = std::make_shared<Static>("");
 
-    auto capacityWarning =
-        std::make_shared<Paragraph>("Disk capacity must be correctly detected for a successful recovery.\n"
-                                    "If a disk listed above has an incorrect size, check HD jumper settings and BIOS "
-                                    "detection, and install the latest OS patches and disk drivers.");
+        for (auto &disk : list_disk)
+        {
+            auto diskBtn = std::make_shared<Button>(disk->description_short(disk));
+            diskBtn->alignment = Alignment::Left;
+            diskBtn->on_hover = [disk, SerialN](bool hover) {
+                if (hover && disk->serial_no)
+                {
+                    SerialN->set_text(StyledText("Serial number: ").colored(disk->serial_no, Color::Green()));
+                }
+                else
+                {
+                    SerialN->set_text("");
+                }
+            };
+            root->add(diskBtn);
+        }
 
-    root->add(note);
-    root->add(capacityWarning);
+        root->add(std::make_shared<VerticalSpacer>());
+
+        root->add(SerialN);
+
+#if __has_include("unistd.h") && !defined(__CYGWIN__) && !defined(__MINGW32__) && !defined(DJGPP)
+        if (geteuid() != 0)
+        {
+            auto noRootWarn = std::make_shared<Static>(StyledText("Note: ").colored_bold(
+                "Some disks won't appear unless you are root user.", Theme::current().warning));
+            root->add(noRootWarn);
+        }
+#endif
+
+        auto capacityWarning = std::make_shared<Paragraph>(
+            "Disk capacity must be correctly detected for a successful recovery.\n"
+            "If a disk listed above has an incorrect size, check HD jumper settings and BIOS "
+            "detection, and install the latest OS patches and disk drivers.");
+
+        root->add(capacityWarning);
+    }
 
     app.run(root);
 }
