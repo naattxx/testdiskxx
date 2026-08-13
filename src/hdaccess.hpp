d6a1b808069f2c29fc5441e39ea640b75f6bd49f
naattxx <31317800+naattxx@users.noreply.github.com>
2026-08-13 18:09:21 +0300

    remove decleration

--- src/hdaccess.hpp
+++ src/hdaccess.hpp
@@ -33,15 +33,3 @@ void hd_parse(list_disk_t &list_disk, const int verbose, const int testdisk_mode
   @ ensures  \result==\null || valid_disk(\result);
   @*/
 std::optional<disk_t> file_test_availability(const char *device, const int verbose, const int testdisk_mode);
-
-/*@
-  @ requires \valid(disk_car);
-  @ requires valid_disk(disk_car);
-  @ requires 0 < disk_car->geom.heads_per_cylinder;
-  @ requires 0 < disk_car->geom.sectors_per_head;
-  @ requires 0 < disk_car->sector_size;
-  @ ensures 0 < disk_car->geom.cylinders < 0x2000000000000;
-  @ ensures  valid_disk(disk_car);
-  @ assigns disk_car->disk_real_size, disk_car->geom.cylinders, disk_car->disk_size;
-  @*/
-void update_disk_car_fields(disk_t &disk_car);
